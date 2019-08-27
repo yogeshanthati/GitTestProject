a97412412b98a3436669c0ad76d4a4e87ad2ab9b
yogeshanthati <yogesh946@gmail.com>
2019-08-27 12:56:50 +0530

    user2code

--- SimpleGitTest/src/main/java/GitTest/SimpleGitTest/App.java
+++ SimpleGitTest/src/main/java/GitTest/SimpleGitTest/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! master code existing here" );
+        System.out.println( "Hello World! user2 code existing here" );
     }
 }
