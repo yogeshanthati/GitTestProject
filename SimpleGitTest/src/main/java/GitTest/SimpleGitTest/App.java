2a718c5c8c5ece6c70c10029721bb4e2cfb8947e
yogeshanthati <yogesh946@gmail.com>
2019-08-28 10:26:56 +0530

    user2code update

--- SimpleGitTest/src/main/java/GitTest/SimpleGitTest/App.java
+++ SimpleGitTest/src/main/java/GitTest/SimpleGitTest/App.java
@@ -11,7 +11,7 @@ public class App
         System.out.println( "Hello World! user2 code existing here" );
         System.out.println( "Hello World! user1code existing here" );
 	System.out.println( "Hello World! user2 code updated existing here" );
-	System.out.println( "Hello World! user2 code updated existing here" );
+	
 
 
     }
